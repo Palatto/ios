df38d1a808a67fb8ef2142cfd8c2f3618336c10c
Nicolas Machado <33378384+nmachado26@users.noreply.github.com>
2018-01-22 13:28:40 -0800

    Color changes for final MVP

--- PlateiOS/PromotionList/Complements/Cell/PromotionCell.swift
+++ PlateiOS/PromotionList/Complements/Cell/PromotionCell.swift
@@ -65,8 +65,8 @@ extension PromotionCell {
         time.text = promotionModel.getParsedDateTime()
         
         if(firstClick) {
-            buttonOutlet.setTitle("GOING", for: .normal)
-            buttonOutlet.setTitleColor(PlateColors.mainGreen, for: .normal)
+            buttonOutlet.setTitle("GO", for: .normal)
+            buttonOutlet.setTitleColor(PlateColors.mainBlue, for: .normal)
         }else {
             buttonOutlet.setTitle("IS THE FOOD OVER?", for: .normal)
             buttonOutlet.setTitleColor(PlateColors.mainLightBlue, for: .normal)
