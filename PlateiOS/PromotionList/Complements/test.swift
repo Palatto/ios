e2f04a8f9389cecfa3140c509a8358c09b197698
Nicolas Machado <33378384+nmachado26@users.noreply.github.com>
2018-02-07 23:51:17 -0800

    test with new test class in feature/test2

---
+++ PlateiOS/PromotionList/Complements/test.swift
@@ -0,0 +1,15 @@
+//
+//  test.swift
+//  PlateiOS
+//
+//  Created by Nick Machado on 2/7/18.
+//  Copyright © 2018 Renner Leite Lucena. All rights reserved.
+//
+
+import Foundation
+
+final class test {
+
+let intX = 35
+
+}
